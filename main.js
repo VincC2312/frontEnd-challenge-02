0f3560bf8a949709bf9983fe3950d0de0154a228
John Alvin <johnalvin.bonifacio@proton.me>
2022-08-31 18:05:42 +0800

    add function for validation email

---
+++ main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const errorMessage = document.querySelector(".home__error-message");
+const errorIcon = document.querySelector(".hm__error-icon");
+const emailInput = document.getElementById("email-input");
+const submitBtn = document.querySelector(".hm__submit-button");
+const form = document.querySelector(".home__form");
+const hidden = document.querySelectorAll(".hidden");
+const re =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+form.addEventListener("submit", function (e) {
+  const email = emailInput.value;
+  e.preventDefault();
+  if (email.match(re)) {
+    errorIcon.classList.add("hidden");
+    errorMessage.classList.add("hidden");
+    form.submit();
+  } else {
+    errorIcon.classList.remove("hidden");
+    errorMessage.classList.remove("hidden");
+  }
+});
+
+// function validateEmail(email) {
+//   const re =
+//     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(String(email).toLowerCase());
+// }
